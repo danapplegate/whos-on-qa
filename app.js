67e48cff56e0d9c7fbb1898a5e444efc37a6b0ef
Dan Applegate <dan@skillshare.com>
2014-09-09 03:53:17 -0400

    Use EDT for time formatting

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/outgoing', function(req, res) {
                 namePath = usernameParts.pop(),
                 userName = namePath.substring(5),
                 created = new Date(deployment.CreatedAt),
-                createdString = created.toLocaleString('en-US', {timeZone: 'America/New_York'});
+                createdString = created.toLocaleString('en-US', {timeZone: 'EDT'});
             responseLines.push(stackName + ': user ' + userName + ' since ' + createdString);
             stacksToProcess = _.without(stacksToProcess, stackId);
         });

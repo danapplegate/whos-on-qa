b65bfbf684373cd5126722fd0b9eee75fe0b051e
Dan Applegate <dan@skillshare.com>
2014-09-09 02:05:50 -0400

    Initial attempt at getting OpsWorks deployment data

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-var Slackhook = require('slackhook');
 var express = require('express');
 var bodyParser = require('body-parser');
+var Slackhook = require('slackhook');
+var AWS = require('aws-sdk');
+
 var app = express();
 
 var slack = new Slackhook({
     domain: process.env.SLACKHOOK_DOMAIN,
     token: process.env.SLACKHOOK_TOKEN
 });
+AWS.config.update({region: 'us-east-1'});
+var opsworks = new AWS.OpsWorks();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/outgoing', function(req, res) {
-    console.log(req.body);
     var hook = slack.respond(req.body);
-    res.json({text: 'Hi ' + hook.user_name, username: hook.user_name});
+    var config = {
+        StackId: 'c7dffdc5-7a1a-4968-8d23-1221457e1449'
+    };
+    opsworks.describeDeployments(config, function(err, data) {
+        res.json({
+            text: err || data.Deployments,
+            username: hook.user_name
+        });
+    });
 });
 
 var server = app.listen(app.get('port'), function() {

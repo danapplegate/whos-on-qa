990d6933cfa26ed34cfdd083205e2a76db5a39e9
Dan Applegate <dan@skillshare.com>
2014-09-12 10:36:47 -0400

    Complete request by responding to slack

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.post('/outgoing', function(req, res) {
                         ]
                     }]
                 });
+                res.send();
             }
         });
         opsReq.send();
